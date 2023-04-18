5a1302405d267daf4f363bf5c48d27185a737a83
abdallahgamal110a <abdallahgamal840@gmail.com>
2023-04-18 15:02:37 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef Dog_H
+#define Dog_H
+
+/**
+ * Struct dog - a dog's basic info
+ * @name : First member
+ * @age : Second member
+ * @owner : Third member
+ * Description : longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
