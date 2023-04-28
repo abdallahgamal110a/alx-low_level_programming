b0080141d61c4d62664debec472d13eaa01304d7
abdallahgamal110a <abdallahgamal840@gmail.com>
2023-04-28 16:07:01 -0700

    space

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -6,6 +6,8 @@
  * @str: String.
  * @len: length of the string.
  *
+ * Description: singly linked list node structure
+ * for Holberton project
  * @next: points to the next node.
  */
 typedef struct list_s
@@ -13,8 +15,8 @@ typedef struct list_s
 	char *str;
 	unsigned int len;
 	struct list_s *next;
-} list_t;
-
+}
+list_t;
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
