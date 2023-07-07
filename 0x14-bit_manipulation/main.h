08614bcce7633e085a0914de460952b797a1e60a
abdallahgamal110 <abdallahgamal840@gmail.com>
2023-07-08 02:33:06 +0300

    1-print_binary.c

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,6 +2,7 @@
 #define MAIN_HEADER
 #include<stdio.h>
 #include<limits.h>
+#include <unistd.h>
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
