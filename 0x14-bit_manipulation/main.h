adfb0fe47f3bc4f624e88d774a301f1ce83541b7
abdallahgamal110 <abdallahgamal840@gmail.com>
2023-07-08 01:41:48 +0300

    header

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_HEADER
 #define MAIN_HEADER
-
+#include<stdio.h>
+#include<limits.h>
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
