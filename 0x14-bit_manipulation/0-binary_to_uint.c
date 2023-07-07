b2d1bfb2ce696eaad9d347006a925f3b57790c3f
abdallahgamal110 <abdallahgamal840@gmail.com>
2023-07-08 01:04:11 +0300

    update

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -4,14 +4,16 @@
  * binary_to_uint - function that converts a binary number to an unsigned int
  * @b: the binary number as a string
  *
- * Return : number
+ * Return : number or 0
  */
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int n;
+unsigned int n = 0;
 
 if (b == NULL)
+{
 return (0);
+}
 while (*b)
 {
 if (*b != '1' && *b != '0')
@@ -19,8 +21,8 @@ if (*b != '1' && *b != '0')
 return (0);
 }
 else
-n = n * 2 + (*b++ - '0')
+n = n * 2 + (*b - '0');
+*b++;
 }
 return (n);
 }
-
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,9 +1,5 @@
 #ifndef MAIN_HEADER
 #define MAIN_HEADER
-<<<<<<< HEAD
-=======
-#include<stdio.h>
->>>>>>> f3b7b80197e4f1fdfa94d85597a1f6da31969119
 
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
@@ -15,7 +11,3 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
 
 #endif
-<<<<<<< HEAD
-=======
-
->>>>>>> f3b7b80197e4f1fdfa94d85597a1f6da31969119
