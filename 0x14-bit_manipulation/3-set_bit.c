a44501e3c295e0dc2c3006e1b97aebb31b981b04
abdallahgamal110 <abdallahgamal840@gmail.com>
2023-07-08 03:00:19 +0300

    nn

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -8,11 +8,11 @@
 */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-unsigned long int A;
-
 if (index > 63)
 return (-1);
-A = 1 << index
-return ((n & A) > 0);
+while (*n = *n | 1ul << index)
+{
+return (1);
+}
 
 }
---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,11 @@
+#include"main.h"
+/**
+* clear_bit - set the value of a bit to 0 at a given index
+* @index: index position to change
+* @n: pointer to decimal number to change
+* Return: 1 if it worked, -1 if error
+*/
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+
+}
