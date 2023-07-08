c1d48ca29f0f7387b462e4430d06102d11850801
abdallahgamal110 <abdallahgamal840@gmail.com>
2023-07-08 03:07:53 +0300

    dd

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -10,9 +10,9 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 if (index > 63)
 return (-1);
-while (*n = *n | 1ul << index)
-{
+if (*n & (1ul << index))
 return (1);
-}
 
+*n = *n | (1ul << index);
+return (1);
 }
