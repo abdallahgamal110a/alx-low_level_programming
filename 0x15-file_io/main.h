8cdfbbd421a14e672a95a700eea31278eb07ff54
abdallahgamal110 <abdallahgamal840@gmail.com>
2023-07-10 23:42:37 +0300

    main header

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,9 +4,11 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-
-#endif /* MAIN_HEADER */
+/* MAIN_HEADER */
+#endif 
