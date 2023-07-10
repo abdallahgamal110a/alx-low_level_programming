d3fbed3447490cad0f7f46a44b80fb0c812d11a3
abdallahgamal110 <abdallahgamal840@gmail.com>
2023-07-10 23:59:50 +0300

    dd

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,6 +7,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+ * File: main.h
+ * Desc: Header file containing prototypes for all functions
+ *       written in the 0x14-file_io directory.
+ */
+
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
