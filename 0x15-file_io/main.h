9d41df9f17c92e3e3107cb3e31a7e233c1ed20e2
abdallahgamal110 <abdallahgamal840@gmail.com>
2023-07-11 03:08:55 +0300

    3-cp.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -12,7 +12,8 @@
  * Desc: Header file containing prototypes for all functions
  *       written in the 0x14-file_io directory.
  */
-
+char *create_buffer(char *file);
+void close_file(int fd);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
