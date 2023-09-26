f8c59083d7165deeecee78f601cf72542a290c08
Abdallah Gamal Hassan <119704743+abdallahgamal110a@users.noreply.github.com>
2023-09-26 21:56:55 +0300

    Delete 0x10-variadic_functions directory

--- 0x10-variadic_functions/variadic_functions.h
+++
@@ -1,9 +0,0 @@
-#ifndef VARI_FUN_H
-#define VARI_FUN_H
-#include <stdio.h>
-#include <stdarg.h>
-void print_all(const char * const format, ...);
-int sum_them_all(const unsigned int n, ...);
-void print_numbers(const char *separator, const unsigned int n, ...);
-void print_strings(const char *separator, const unsigned int n, ...);
-#endif
