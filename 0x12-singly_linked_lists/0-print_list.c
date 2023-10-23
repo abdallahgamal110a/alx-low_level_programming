349bea512e36f1bbf3fe177cd93768088704a021
abdallahgamal110 <abdallahgamal840@gmail.com>
2023-10-23 22:07:49 +0300

    A

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,20 +9,24 @@
  */
 size_t print_list(const list_t *h)
 {
-size_t n = 0;
+	size_t count = 0;
 
-while (h)
-{
-	if (!h->str)
+	if (h == NULL)
 	{
-		printf("[0] (nil)\n");
+		return (0);
 	}
 	else
 	{
-		rintf("[%u] %s\n", h->len, h->str);
+		while (h != NULL)
+		{
+			if (h->str == NULL)
+			printf("[0] (nil)\n");
+			else
+			printf("[%d] %s\n", h->len, h->str);
+
+			h = h->next;
+			count++;
+		}
 	}
-	h = h->next;
-	n++;
-}
-return (n);
+	return (count);
 }
